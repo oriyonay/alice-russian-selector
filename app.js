765a75f63dd599720c08c401aa33b7cb231e783a
Ori Yonay <oriyonay12@gmail.com>
2025-02-13 12:47:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const studentData = {
     section1: [ // section 501
         "Michael", "Abby", "Max", "Ashlyn", "Christian", 
         "Cooper", "Isaac", "Jack", "Andrew", "Niya", "Litzie", 
-        "Nathan", "Carmen", "Jacqueline", "Ian", "Keaton", 
+        "Nathan", "Carmen", "Jacqueline", "Keaton",  // no Ian
         "Christopher", "Lane", "Grace", "Destiny", "Alim", 
         "Joshua", "Luca"
     ],
